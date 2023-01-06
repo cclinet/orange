da4e29b233b2be225c6b69c8ee04e412200ab651
cclin <cclinet@outlook.com>
2023-01-06 18:09:24 +0800

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,25 @@ import Image from "next/image";
 import Head from "next/head";
 import { useState } from "react";
 
+const paths: string[] = ["model", "posts"];
+
+function NavMenuList() {
+  return (<>
+    {paths.map((path, index) => (
+      <li key={index}>
+        <Link
+          className="text-gray-500 transition hover:text-gray-500/75"
+          href={`/${path}`}
+        >
+          {path}
+        </Link>
+      </li>
+    ))}
+  </>)
+}
 export default function NavBar({ children }: any) {
   const [menuVisible, setMenuVisible] = useState(false);
-  const paths: string[] = ["model", "posts"];
+
 
   return (
     <>
@@ -14,6 +30,7 @@ export default function NavBar({ children }: any) {
         <meta name="description" content="orange" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <header aria-label="Site Header" className="bg-white">
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
           <Link className="block text-teal-600" href="/">
@@ -40,16 +57,7 @@ export default function NavBar({ children }: any) {
           <div className="flex flex-1 items-center justify-end md:justify-between">
             <nav aria-label="Site Nav" className="hidden md:block">
               <ul className="flex items-center gap-6 text-sm">
-                {paths.map((path, index) => (
-                  <li key={index}>
-                    <Link
-                      className="text-gray-500 transition hover:text-gray-500/75"
-                      href={`/${path}`}
-                    >
-                      {path}
-                    </Link>
-                  </li>
-                ))}
+                <NavMenuList/>
               </ul>
             </nav>
 
@@ -95,15 +103,9 @@ export default function NavBar({ children }: any) {
                   }
                   role="menu"
                 >
-                  <div className="p-2">
-                    <Link
-                      href="/model"
-                      className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
-                      role="menuitem"
-                    >
-                      model
-                    </Link>
-                  </div>
+                  <ul>
+                  <NavMenuList/>
+                  </ul>
                 </div>
               </button>
             </div>
