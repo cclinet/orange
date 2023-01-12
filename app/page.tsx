9083876b3102945f0db16b0c1472b620abc5c61f
cclin <cclinet@outlook.com>
2023-01-12 15:11:58 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <>
-      <h1 className="text-3xl font-italianno underline">
+      <h1 className="text-3xl font-italianno underline mt-20">
         There is only one heroism in the world : it&apos;s to see the world as
         it is, and to love it.
       </h1>
