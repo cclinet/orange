7808e1a2fe37b17ca2a3daf5be40818fd2d0dfa7
cclin <cclinet@outlook.com>
2023-01-15 22:21:08 +0800

    update

--- app/post/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="grid h-screen px-4 bg-white place-content-center">
-      <h1 className="tracking-widest text-gray-500 uppercase">
-        404 | Not Found
-      </h1>
-    </div>
-  );
-}
